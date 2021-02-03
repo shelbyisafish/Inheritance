c8123113b924ce9d76a4a154505a9be7d6e0f4e8
shelby-desk <shelbyisa9139@gmail.com>
2021-02-02 18:45:03 -0800

    Add project files.

---
+++ InheritanceConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InheritanceConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
